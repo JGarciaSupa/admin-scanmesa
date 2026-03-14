48c6c55e134f87434014eb9bfdef136a8446a7b6
Renato Navarro A. <72818352+devrenatonavarro@users.noreply.github.com>
2026-03-14 12:01:53 -0500

    api url update

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@
 // ============================================================================
 
 const config = {
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'backend-mesascan.lobitoconsulting.com',
   storageKeys: {
     accessToken: 'accessToken',
     user: 'user',
